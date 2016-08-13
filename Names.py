64fceb2ae52f81635cc1391cb4171ac9a9921ae0
McFanta_ <johannes.theigen@gmail.com>
2016-08-13 10:06:33 +0200

    Basic if elif else functions and readd practice project!

---
+++ Names.py
@@ -0,0 +1,10 @@
+name = "Johannes"
+
+if name is "Johannes":
+    print("Hey Johannes")
+elif name is "McFanta":
+    print("Hey McFanta")
+elif name is "Fanta":
+    print("Hey Fanta")
+else:
+    print("Please sign up for the site!")
