ecd405fffd4cbde060bbce3d6bbc27005d50f14f
McFanta_ <johannes.theigen@gmail.com>
2016-08-15 16:58:28 +0200

    Learning Functions!

---
+++ Practice/functions.py
@@ -0,0 +1,19 @@
+def one():
+    print("1")
+one()
+
+def two():
+    print("2")
+two()
+
+def three():
+    print("3")
+three()
+
+def four():
+    print("4")
+four()
+
+def five():
+    print("5")
+five()
