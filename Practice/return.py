73e7bcebdf988bc1c5320c9bfff16208708bba52
McFanta_ <johannes.theigen@gmail.com>
2016-08-18 10:53:10 +0200

    Learning return values

---
+++ Practice/return.py
@@ -0,0 +1,6 @@
+def allowed_dating_age(my_age):
+    girls_age = my_age/2 + 6
+    return girls_age
+
+johannes_limit = allowed_dating_age(14)
+print("Johannes can date", johannes_limit, "year old girls or older")
