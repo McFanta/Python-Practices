2e04602c8a01e9aadd6d1188f9cdce651e5e57c2
McFanta_ <johannes.theigen@gmail.com>
2016-08-18 11:01:13 +0200

    Add a comment to return.py

--- Practice/return.py
+++ Practice/return.py
@@ -1,5 +1,7 @@
+# A funny program that calculates the allowed age limit of girls I can date
+
 def allowed_dating_age(my_age):
-    girls_age = my_age/2 + 6
+    girls_age = my_age / 2 + 6
     return girls_age
 
 johannes_limit = allowed_dating_age(14)
