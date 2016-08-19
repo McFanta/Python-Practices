46d3d6113d0039d1bbc708d8e3ea2691c4451a16
McFanta_ <johannes.theigen@gmail.com>
2016-08-19 19:50:36 +0200

    Add a program that tells you what your favorite color is.

---
+++ Practice/favoritecolor.py
@@ -0,0 +1,10 @@
+# A program that tells you what your favorite color is
+
+def your_favorite_color():
+    color = "green!"
+    c = color
+    if c is color:
+            print("Your favorite color is", color)
+    else:
+        print("You have none favorite colors!")
+your_favorite_color()
