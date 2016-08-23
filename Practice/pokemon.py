1ef329198c006a88a5db74df79ca9b01124cbdb8
McFanta_ <johannes.theigen@gmail.com>
2016-08-23 08:53:02 +0200

    Add a program that tells you what starter pokèmon you have chosen!

---
+++ Practice/pokemon.py
@@ -0,0 +1,9 @@
+# A program that tells you what starter pokèmon you have chosen
+
+def your_starter_pokèmon():
+    pokèmon = "Squirtle!"
+    if pokèmon is 'Squirtle!':
+        print("Your starter pokèmon is", pokèmon)
+    elif pokèmon is 'Unknown':
+        print("You don't have any starter pokèmons!")
+your_starter_pokèmon()
