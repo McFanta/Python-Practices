c7d51d6fb3b59f600823ef5b301046ebe54c5863
Johannes <johannes.theigen@gmail.com>
2016-09-11 10:46:36 +0200

    Add a comment to keyword.py

--- Practice/keyword.py
+++ Practice/keyword.py
@@ -1,3 +1,5 @@
+# A simple program that I made to learn the basics about keyword arguments
+
 def dumb_sentence(name="Johannes", action="ate", item="pizza."):
     print name, action, item
 dumb_sentence()
