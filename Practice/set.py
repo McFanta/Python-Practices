1a5840d3b9b15b54161dc4d4a97b003c336bcfe6
mcfanta <johannes.theigen@gmail.com>
2016-09-25 21:35:24 +0200

    Learning the basics of set

---
+++ Practice/set.py
@@ -0,0 +1,8 @@
+# Just a simple program that I made to learn how the set function works
+
+groceries = {'cereal', 'milk', 'jam', 'bread', 'egg', 'milk'}
+print(groceries)
+if 'milk' in groceries:
+    print("Nice you have milk.")
+else:
+    print("You need to buy milk.")
