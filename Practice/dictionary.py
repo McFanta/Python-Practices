be083d07b1bb86df34b4ed19e2b4eb413a8318a6
mcfanta <johannes.theigen@gmail.com>
2016-10-07 19:19:24 +0200

    Learning about dictionaries in python!

---
+++ Practice/dictionary.py
@@ -0,0 +1,6 @@
+# A program that I made to learn about dictionaries in python
+
+hobbies = {'Programming': ' is my favorite hobby!', 'Rubik\'s Cube': ' is a fun hobby.', 'Music': ' is a fun hobby!'}
+
+for k, v in hobbies.items():
+    print(k + v)
