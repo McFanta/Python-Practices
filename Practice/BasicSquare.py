074d5154c460dece30c83ba104411b636d5eef76
McFanta <johannes.theigen@gmail.com>
2016-10-20 21:57:49 +0200

    Add another turtle graphics program

---
+++ Practice/BasicSquare.py
@@ -0,0 +1,16 @@
+# A basic square in turtle graphics
+
+import turtle
+from time import sleep
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.setup(800, 600)
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+wn.title("A basic square")
+turtle.getscreen()._root.mainloop()
